a9e304eab7088625230fa3e9e3d1191725094fc6
Aditya bandaru <adityabandaruuu18@gmail.com>
2025-03-10 15:47:19 +0530

    2 question added

--- Lab-6/2.py
+++ Lab-6/2.py
@@ -1,5 +1,9 @@
 a = 182841384165841685416854134135
 b = 135481653441354138548413384135
-m = pow(10,9)+7
+m = 10**9 + 7 
 
-print("Modulus of a and b is: ", (a/b)%m)
+if pow(b, -1, m):
+    result = (a * pow(b, -1, m)) % m
+    print("Modular Division Result:", result)
+else:
+    print("Modular inverse does not exist.")
